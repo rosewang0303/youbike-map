4feed639f81f54faeb9bca652baa5a20d8fda2da
rose <rosewang0303@gmail.com>
2021-11-16 01:57:07 +0800

    init nuxt app

---
+++ components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav class="header">
+        <div></div>
+        <ul>
+            <li>找單車</li>
+            <li>找路線</li>
+            <li>找景點</li>
+        </ul>
+    </nav>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="layout-default">
+        <Header />
+        <nuxt/>
+    </div>
+</template>
